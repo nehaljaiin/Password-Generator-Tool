30cf9bf9b65e900b57ea0cd994850051475b6f51
Nehal <nehaljain270@gmail.com>
2024-06-26 09:12:27 +0530

    Update Password Generator.py

--- Password Generator.py
+++ Password Generator.py
@@ -1,1 +1,19 @@
+#import library
+import random
 
+#define combinations
+lowers = 'abcdefghijklmnopqrstuvwxyz'
+uppers = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+numbers = '012345678'
+symbols = '!@#$%^&*'
+
+combination = lowers + uppers + numbers + symbols
+
+#fix the length of password
+length = 8
+
+#generating the password
+password = ''.join(random.sample(combination, length))
+
+#here the password you got!
+print(password) 
